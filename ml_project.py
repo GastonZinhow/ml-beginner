06bd62db3fa6dd3747e538e2c5838bc86a17abda
Matheus Gaston <theteusviana@gmail.com>
2025-06-09 15:16:21 -0300

    feat: webapplcation with linear regression model

---
+++ ml_project.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("pizzas.csv")
+
+model = LinearRegression()
+x = df[["diametro"]]
+y = df[["preco"]]
+
+model.fit(x, y)
+
+print(model.predict([[80]])[0][0])
+
+st.title("Prevendo o valor de uma pizza")
+st.divider()
+
+diametro = st.number_input("Digite o tamanho do diametro da pizza: ")
+
+if diametro:
+    preco_previsto = model.predict([[diametro]])[0][0]
+    st.write(f"O valor da pizza com diâmetro de {diametro:.2f}cm é de R${preco_previsto:.2f}.")
